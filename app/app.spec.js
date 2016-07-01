0b696b08ef186c288b40493cb0a006c548742a39
Santiago Basulto <santiago.basulto@gmail.com>
2016-07-01 16:45:50 -0300

    App component + testing + better eslint

---
+++ app/app.spec.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Base TestCase', () => {
+  it('1 + 1 should be 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom'
+import { render } from 'react-dom';
+
+import App from './app/App';
 
 render(
-  <h1>Hello World</h1>,
+  <App />,
   document.getElementById('app')
 );
